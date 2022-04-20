c3f9e1f79ff0f20a0bbe993e1d776821307835d1
Ioanna <ioanna.kozeljp@gmail.com>
2022-04-20 11:40:02 +0200

    Add project files.

---
+++ RaveDataProject/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace RaveDataProject
+{
+    public partial class RaveApp : Form
+    {
+        public RaveApp()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            MySqlConnection cnn;
+            connetionString = @"Data Source=localhost; Initial Catalog=mydb ;User ID=root;Password=root";
+
+            cnn = new MySqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open!");
+            cnn.Close();
+        }
+
+        private void RaveApp_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
